375582eec552b8f68a91c589493cb2d60177f4bf
Vincent <vincentckhuang@gmail.com>
2016-08-11 14:35:13 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
